1b3b6590cbeee1bd6e922217fdcd679be0edde52
Samraat <samraatbansal7@gmail.com>
2021-08-12 20:10:10 +0530

    mongo

--- config/mongo_client.js
+++ config/mongo_client.js
@@ -1,18 +1,18 @@
-const {MongoClient} = require('mongodb');
-const env = require("./environment.js");
+// const {MongoClient} = require('mongodb');
+// const env = require("./environment.js");
 
-async function main(){
-    const uri = env.db_uri;
+// async function main(){
+//     const uri = env.db_uri;
 
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
 
-    try {
-        await client.connect();
+//     try {
+//         await client.connect();
         
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.close();
-    } 
-}
-main().catch(console.error);
+//     } catch (err) {
+//         console.error(err);
+//     } finally {
+//         await client.close();
+//     } 
+// }
+// main().catch(console.error);
