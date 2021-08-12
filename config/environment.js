8d31a9022bbb3bf360fbb63ba42208b349481059
Samraat <samraatbansal7@gmail.com>
2021-08-12 18:09:53 +0530

    remote db

--- config/environment.js
+++ config/environment.js
@@ -17,14 +17,15 @@ const development = {
     name: 'development',
     asset_path: 'static',
     // session_cookie_key: 'blahsomething',
-    db: 'mongodb+srv://anmol:anmol123@cluster0.myzqs.mongodb.net',
+    db: 'mongodb://116.202.96.218/TBS_main_db?authSource=admin',
     db_uri: "mongodb+srv://anmol:anmol123@cluster0.myzqs.mongodb.net/keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000?ssl=true&ssl_cert_reqs=CERT_NONE",
     google_client_id: "1057271008763-fik1k4g2kl5man96m96qai4hf0ubt0ol.apps.googleusercontent.com",
     google_client_secret: "zDcHEIgtDyc-fJtCFUYM_GGH",
     google_redirect_url: "https://developers.google.com/oauthplayground",
     google_refresh_token: "1//043NIDmwe9C9VCgYIARAAGAQSNwF-L9IrvpLvqPa_D--h6CjcwKc7LYw6x9FzyC4eIn6WJGaq9f3U3UR4oY7LKb4aW_6oDAwp1gk",
-    sendgrid_api_key: "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"
-
+    sendgrid_api_key: "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",
+    mongo_user:"samraat",
+    mongo_pass:"Samraat@TBS2021"
     // google_recaptcha_dateSiteKey: "6LcZNCsbAAAAAKnEucGFf1PJxCFzvqapMy4NtjNz",
     // google_call_back_url: "http://localhost:8000/users/auth/google/callback",
     // jwt_secret: 'codeial',
@@ -41,7 +42,9 @@ const production =  {
     // session_cookie_key: process.env.TBS_SESSION_COOKIE_KEY,
     db: process.env.TBS_DB,
     db_uri: process.env.TBS_DB_URI,
-    sendgrid_api_key: process.env.TBS_SENDGRID
+    sendgrid_api_key: process.env.TBS_SENDGRID,
+    mongo_user: process.env.TBS_MONGO_USER,
+    mongo_pass: process.env.TBS_MONGO_PASS
         // google_client_id: process.env.TBS_GOOGLE_CLIENT_ID,
     // google_client_secret: process.env.TBS_GOOGLE_CLIENT_SECRET,
     // google_redirect_url: process.env.TBS_GOOGLE_REDIRECT_URL,
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const env = require('../config/environment')
 // const mongoURI = "mongodb+srv://samraat:Samraat@7@cluster0.uxnjj.mongodb.net/hiring_block_db?retryWrites=true&w=majority";
 // mongoose.connect('mongodb://localhost/waitlist_db', {useNewUrlParser: true});
-mongoose.connect(env.db, {useNewUrlParser: true});
+
+const mongoURI = env.db;
+let options = {
+    user: env.mongo_user,
+    pass: env.mongo_pass,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(mongoURI, options)
 // // mongoURI || 
 // // || 'mongodb://localhost/hiring_block_db'
 const db = mongoose.connection;
